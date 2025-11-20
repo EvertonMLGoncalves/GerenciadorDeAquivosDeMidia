3db0654f6bcc19baff8f481002a6ad2290c604d1
Eloizasocoloski <eloizasocoloskipereira15@gmail.com>
2025-11-20 18:58:07 -0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/gerenciador_midia/model/Midia.java
+++ src/main/java/gerenciador_midia/model/Midia.java
@@ -1,4 +1,26 @@
 package gerenciador_midia.model;
 
-public class Midia {
+public abstract class Midia {
+
+    protected String local;
+    protected long tamanhoEmDisco;
+    protected String titulo;
+    protected int duracao;
+    protected String categoria;
+
+    public Midia(String local, long tamanhoEmDisco, String titulo, int duracao, String categoria) {
+        this.local = local;
+        this.tamanhoEmDisco = tamanhoEmDisco;
+        this.titulo = titulo;
+        this.duracao = duracao;
+        this.categoria = categoria;
+    }
+
+    public String exibirDetalhes() {
+        return "Título: " + titulo +
+                "\nLocal: " + local +
+                "\nTamanho em disco: " + tamanhoEmDisco +
+                "\nDuração: " + duracao +
+                "\nCategoria: " + categoria;
+    }
 }
