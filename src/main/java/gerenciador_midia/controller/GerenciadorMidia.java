c7ae8f3a2c97d43a8af4e0a4f7e6125b923ab128
Eloizasocoloski <eloizasocoloskipereira15@gmail.com>
2025-11-20 21:06:50 -0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/gerenciador_midia/controller/GerenciadorMidia.java
+++ src/main/java/gerenciador_midia/controller/GerenciadorMidia.java
@@ -9,5 +9,19 @@ public class GerenciadorMidia {
 
     ArrayList<Midia> midias;
 
+    public void incluirMidia(Midia midia){
+        midias.add(midia);
+    }
+    public void EditarMidia(Midia midia){
+        midias.remove(midia);
+        midias.add(midia);
+    }
+    public void excluirMidia(Midia midia){
+        midias.remove(midia);
+    }
+    public void moverMidia(Midia midia){
+
+    }
+
     }
 
