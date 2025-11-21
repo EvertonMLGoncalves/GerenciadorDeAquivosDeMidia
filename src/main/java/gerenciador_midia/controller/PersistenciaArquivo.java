fe2565ebe50ce23833c5c558cc91acdc8fe8780e
Eloizasocoloski <eloizasocoloskipereira15@gmail.com>
2025-11-20 21:23:33 -0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/gerenciador_midia/controller/PersistenciaArquivo.java
+++ src/main/java/gerenciador_midia/controller/PersistenciaArquivo.java
@@ -1,15 +1,20 @@
 package gerenciador_midia.controller;
 
+import java.io.*;
 import java.nio.file.Path;
 
 public class PersistenciaArquivo {
 
     private Path pasta;
-
+    File f = new File("arquivo.txt");
     public PersistenciaArquivo(Path pasta) {
         this.pasta = pasta;
     }
-    public void salvarMidia(Path midia) {
-
+    public void salvarMidia(Path midia) throws IOException {
+        FileOutputStream fos = new FileOutputStream("arquivo");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(midia);
+        oos.close();
     }
-}
+    }
+
