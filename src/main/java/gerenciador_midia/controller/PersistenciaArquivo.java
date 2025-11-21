c72b1e7c03c9d88404d7679a9202fc14ccc45104
Eloizasocoloski <eloizasocoloskipereira15@gmail.com>
2025-11-20 21:43:02 -0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/gerenciador_midia/controller/PersistenciaArquivo.java
+++ src/main/java/gerenciador_midia/controller/PersistenciaArquivo.java
@@ -7,14 +7,24 @@ public class PersistenciaArquivo {
 
     private Path pasta;
     File f = new File("arquivo.txt");
+
     public PersistenciaArquivo(Path pasta) {
         this.pasta = pasta;
     }
+
     public void salvarMidia(Path midia) throws IOException {
-        FileOutputStream fos = new FileOutputStream("arquivo");
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(midia);
-        oos.close();
+        FileOutputStream fos = new FileOutputStream("arquivo.txt");
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(midia);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado.");
+        }catch (IOException e){
+            System.out.println("Erro ao salvar o arquivo.");
+        }
+
+
     }
-    }
+}
 
