47807be83be13fe0452dc4cc8b3b647c1e4021fc
Eloizasocoloski <eloizasocoloskipereira15@gmail.com>
2025-11-23 10:21:21 -0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/gerenciador_midia/controller/TelaPrincipal.java
+++ src/main/java/gerenciador_midia/controller/TelaPrincipal.java
@@ -1,12 +1,6 @@
 package gerenciador_midia.controller;
 
-import gerenciador_midia.model.Midia;
-
 public class TelaPrincipal {
 
-    GerenciadorMidia gerenciadorMidia;
-
-    public void incluirMidia(Midia midia){
-
     }
-}
+
